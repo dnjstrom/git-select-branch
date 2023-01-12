d88253c0abf74e44b6ebae325d9bc8abf251e9ca
Joar Wandborg <joar@wandborg.se>
2023-01-12 17:00:51 +0100

    Replace `git` invocations with git2

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
+extern crate core;
+
+use std::env;
+
+use anyhow::{Context, Result};
 use dialoguer::{theme::ColorfulTheme, Select};
-use std::process::Command;
+use git2::build::CheckoutBuilder;
+use git2::{BranchType, Commit, Reference, Repository};
 
 /// Tiny CLI utility to checkout a recent git branch interactively.
-fn main() -> std::io::Result<()> {
-    let current_branch_output = exec_command("git rev-parse --abbrev-ref HEAD");
-    let current_branch = current_branch_output.trim();
+fn main() -> Result<()> {
+    let current_dir =
+        env::current_dir().with_context(|| "Could not get current directory".to_string())?;
+    let repo = Repository::discover(current_dir.as_path())
+        .with_context(|| format!("No git repository discovered in {:?}", current_dir.clone()))?;
 
-    let all_branches_output = exec_command(
-        "git for-each-ref --count=20 --sort=-committerdate refs/heads/ --format='%(refname:short)'",
-    );
-    let all_branches: Vec<&str> = all_branches_output
-        .lines()
-        .filter(|s| *s != current_branch)
-        .collect();
-
-    let mut options = Vec::new();
-    options.push(current_branch);
-    options.extend(all_branches);
+    let current_branch_owned = get_current_branch(&repo)?;
+    let current_branch = current_branch_owned;
+    let sorted_branches = get_sorted_branches(&repo)?;
+    let options = get_branch_options(sorted_branches, current_branch.as_deref());
 
     let result = Select::with_theme(&ColorfulTheme::default())
         .items(&options)
@@ -28,50 +29,73 @@ fn main() -> std::io::Result<()> {
 
     match result {
         Some(selection) => {
-            let selected_branch = options[selection];
-            let checkout_command = format!("git checkout {}", selected_branch);
-            spawn_command(&checkout_command);
+            let selected_branch = &options[selection];
+            checkout(repo, selected_branch)?;
         }
-        None => println!("Stayed on branch '{}'", current_branch),
+        None => match current_branch {
+            Some(branch) => println!("Stayed on branch '{}'", &branch),
+            None => println!("Doing nothing"),
+        },
     }
 
     Ok(())
 }
 
-/// Executes a command in the appropriate shell of the os, returning the output on stdout.
-fn exec_command(command: &str) -> String {
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(&["/C", command])
-            .output()
-            .expect("failed to execute process")
-    } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg(command)
-            .output()
-            .expect("failed to execute process")
-    };
-
-    String::from_utf8(output.stdout).expect("Can't parse string as utf-8")
+fn checkout(repo: Repository, branch_name: &String) -> Result<()> {
+    let ref_name = format!("refs/heads/{}", branch_name);
+    repo.set_head(&ref_name)
+        .with_context(|| format!("Could not set head to {}", ref_name))?;
+    repo.checkout_head(Some(&mut CheckoutBuilder::default()))
+        .with_context(|| format!("Unable to check out branch {}", branch_name))?;
+    Ok(())
 }
 
-/// Spawns a command transparently in the appropriate shell of the os and waits until complete.
-///
-/// Any command output is redirected to parent stdout/stderr.
-fn spawn_command(command: &str) {
-    let mut child = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(&["/C", command])
-            .spawn()
-            .expect("failed to execute process")
-    } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg(command)
-            .spawn()
-            .expect("failed to execute process")
-    };
+fn get_branch_options(sorted_branches: Vec<String>, current_branch: Option<&str>) -> Vec<String> {
+    let all_branches: Vec<String> = sorted_branches
+        .into_iter()
+        .filter(|s| match current_branch {
+            Some(branch) => *s != branch,
+            None => true,
+        })
+        .collect();
 
-    child.wait().expect("Failed to wait on child");
+    let mut options = Vec::new();
+    match current_branch {
+        Some(branch) => options.push(branch.to_string()),
+        None => options.push("<no branch>".to_string()),
+    }
+    options.extend(all_branches);
+    options
+}
+
+fn get_current_branch(repo: &Repository) -> Result<Option<String>> {
+    Ok(repo
+        .head()
+        .with_context(|| "Can't get repo head".to_string())?
+        .shorthand()
+        .map(|s| s.to_string()))
+}
+
+fn get_sorted_branches(repo: &Repository) -> Result<Vec<String>> {
+    let branch_refs: Vec<Reference> = repo
+        .branches(Some(BranchType::Local))?
+        .filter(|r| r.is_ok())
+        .map(|r| r.unwrap().0.into_reference())
+        .collect();
+    let mut branch_name_and_commit: Vec<(String, Commit)> = branch_refs
+        .iter()
+        .filter(|r| r.shorthand().is_some() && r.peel_to_commit().is_ok())
+        .map(|r| {
+            (
+                r.shorthand().unwrap().to_string(),
+                r.peel_to_commit().unwrap(),
+            )
+        })
+        .collect();
+    branch_name_and_commit.sort_by(|(_, a), (_, b)| a.time().partial_cmp(&b.time()).unwrap());
+    Ok(branch_name_and_commit
+        .iter()
+        .take(20)
+        .map(|(name, _)| name.to_string())
+        .collect())
 }
