02ed60a395c1097459f9e5ef0f7b4bf8011642da
Joar Wandborg <joar@wandborg.se>
2023-01-18 08:42:57 +0100

    Add options, and features
    with the power of `git2` we can add lots of configurable options through
    git's config files!
    
    I started with these:
    
    - `select-branch.theme`: `"colorful" | "simple"`
    - `select-branch.limit`: `1..usize::MAX | "none"`
    - `select-branch.fuzzy`: `"true" | "false"`
    
    as well as implement the things that change based on those configuration
    options.
    
    I also made an attempt to work around the issue described in
    https://github.com/console-rs/dialoguer/issues/188, the workaround isn't
    perfect, but making it more perfect might require modifying the way
    `term.read_key()`'s `Err` case is handled in e.g.:
    - https://github.com/console-rs/dialoguer/blob/f6f6e260142e30d2f750a0f25e76191a6a0e4223/src/prompts/select.rs#L275 and
    - https://github.com/console-rs/dialoguer/blob/f6f6e260142e30d2f750a0f25e76191a6a0e4223/src/prompts/fuzzy_select.rs#L225

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::env;
+extern crate core;
 
-use anyhow::Result;
-use dialoguer::{theme::ColorfulTheme, Select};
+use std::convert::TryFrom;
+use std::io::ErrorKind;
+use std::{env, usize};
+
+use anyhow::{anyhow, Context, Result};
+use dialoguer::theme::{ColorfulTheme, SimpleTheme, Theme};
+use dialoguer::{FuzzySelect, Select};
 use expect_exit::Expected;
 use git2::{BranchType, Commit, Reference, Repository};
 
@@ -11,34 +16,81 @@ fn main() -> Result<()> {
     let repo = Repository::discover(current_dir.as_path())
         .or_exit_(format!("No git repository discovered at {:?}", current_dir).as_str());
 
-    let current_branch_owned = get_current_branch(&repo)?;
-    let current_branch = current_branch_owned;
-    let sorted_branches = get_sorted_branches(&repo)?;
+    let git_config = repo
+        .config()
+        .with_context(|| "Could not get git config")?
+        .snapshot()
+        .with_context(|| "Could not create a snapshot of git config")?;
+    let config = Config::from_git_config(&git_config)
+        .with_context(|| "Error reading configuration from git")?;
+
+    let current_branch = get_current_branch(&repo)?;
+    let sorted_branches = get_sorted_branches(&config, &repo)?;
     let options = get_branch_options(
         sorted_branches.iter().map(|s| s.as_str()).collect(),
         current_branch.as_deref(),
     );
 
-    let result = Select::with_theme(&ColorfulTheme::default())
-        .items(&options)
-        .paged(true)
-        .default(0)
-        .with_prompt("Which branch would you like to switch to?")
-        .interact_opt()
-        .expect("No selection");
+    ctrlc::set_handler(move || {
+        dialoguer_reset_cursor_hack();
+    })?;
 
-    match result {
-        Some(selection) => {
-            let selected_branch = &options[selection];
-            checkout(repo, selected_branch)?;
-        }
-        None => match current_branch {
-            Some(branch) => println!("Stayed on branch '{}'", &branch),
-            None => println!("Doing nothing"),
+    let prompt_result = match config.fuzzy {
+        true => FuzzySelect::with_theme(config.theme.as_ref())
+            .items(&options)
+            .default(0)
+            .with_prompt("Which branch would you like to switch to?")
+            .interact_opt()
+            .with_context(|| "Prompt interrupted"),
+        false => Select::with_theme(config.theme.as_ref())
+            .items(&options)
+            .default(0)
+            .with_prompt("Which branch would you like to switch to?")
+            .interact_opt()
+            .with_context(|| "Prompt interrupted"),
+    };
+
+    match prompt_result {
+        Ok(option) => match option {
+            Some(selection) => {
+                let selected_branch = &options[selection];
+                checkout(repo, selected_branch)?;
+                Ok(())
+            }
+            None => Err(anyhow!("Aborted.")),
+        },
+        // If err, figure out if it was a SIGINT and ...
+        Err(err) => match err.downcast_ref::<std::io::Error>() {
+            Some(io_err) => match io_err.kind() {
+                // ... if so replace err with a shorter version.
+                ErrorKind::Interrupted => Err(anyhow!("Interrupted")),
+                _ => Err(err),
+            },
+            None => Err(err),
         },
     }
+}
 
-    Ok(())
+/// `dialoguer` doesn't clean up your term if it's aborted via e.g. `SIGINT` or other exceptions:
+/// https://github.com/console-rs/dialoguer/issues/188.
+///
+/// `dialoguer`, as a library, doesn't want to mess with signal handlers,
+/// but we, as an application, are free to mess with signal handlers if we feel like it, since we
+/// own the process.
+fn dialoguer_reset_cursor_hack() {
+    let term = dialoguer::console::Term::stdout();
+    let _ = term.show_cursor();
+}
+
+fn match_theme_config(theme_name: &str) -> Result<Box<dyn Theme>> {
+    match theme_name {
+        "colorful" => Ok(Box::new(ColorfulTheme::default())),
+        "simple" => Ok(Box::new(SimpleTheme)),
+        value => Err(anyhow!(
+            "{} is not a valid theme, expected one of \"colorful\", \"simple\"",
+            value
+        )),
+    }
 }
 
 fn checkout(repo: Repository, branch_name: &String) -> Result<()> {
@@ -83,7 +135,7 @@ fn get_current_branch(repo: &Repository) -> Result<Option<String>> {
         .map(|s| s.to_string()))
 }
 
-fn get_sorted_branches(repo: &Repository) -> Result<Vec<String>> {
+fn get_sorted_branches(config: &Config, repo: &Repository) -> Result<Vec<String>> {
     let branch_refs: Vec<Reference> = repo
         .branches(Some(BranchType::Local))?
         .filter(|r| r.is_ok())
@@ -104,12 +156,11 @@ fn get_sorted_branches(repo: &Repository) -> Result<Vec<String>> {
     branch_name_and_commit.sort_by(|(_, a), (_, b)| a.time().partial_cmp(&b.time()).unwrap());
     branch_name_and_commit.reverse();
 
-    let branches = branch_name_and_commit
-        .iter()
-        .take(20)
-        .map(|(name, _)| name.to_string())
-        .collect();
-
+    let iter = branch_name_and_commit.iter();
+    let branches = match config.limit {
+        Some(limit) => iter.take(limit).map(|(name, _)| name.to_string()).collect(),
+        None => iter.map(|(name, _)| name.to_string()).collect(),
+    };
     Ok(branches)
 }
 
@@ -183,4 +234,75 @@ mod tests {
         let options = get_branch_options(vec!["a", "b", "c"], Some("c"));
         assert_eq!(options, vec!["c", "a", "b"])
     }
+}
+
+struct Config {
+    theme: Box<dyn Theme>,
+    fuzzy: bool,
+    limit: Option<usize>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            theme: Box::new(ColorfulTheme::default()),
+            fuzzy: false,
+            limit: Some(20usize),
+        }
+    }
+}
+
+impl Config {
+    fn from_git_config(git_config: &git2::Config) -> Result<Config> {
+        let mut config = Config::default();
+        if let Some(value) = map_git2_not_found_to_none(git_config.get_bool("select-branch.fuzzy"))
+            .with_context(|| "Error parsing select-branch.fuzzy")?
+        {
+            config.fuzzy = value;
+        }
+
+        if let Some(value) = map_git2_not_found_to_none(git_config.get_str("select-branch.theme"))
+            .with_context(|| "Error parsing select-branch.theme")?
+        {
+            config.theme =
+                match_theme_config(value).with_context(|| "Could not parse theme configuration")?;
+        }
+
+        if let Some("none") = map_git2_not_found_to_none(git_config.get_str("select-branch.limit"))
+            .with_context(|| "Error parsing select-branch.limit")?
+        {
+            config.limit = None
+        } else if let Some(limit) =
+            map_git2_not_found_to_none(git_config.get_i64("select-branch.limit"))
+                .with_context(|| "Error parsing select-branch.limit")?
+        {
+            if limit <= 0 {
+                return Err(anyhow!(
+                    "\"{}\" is not a valid \"select-branch.limit\" value.\n\
+                    The value must be either a positive integer, or \"none\". e.g.:\n\
+                    > git config --global select-branch.limit none\n\
+                    or\n\
+                    > git config --global select-branch.limit 20",
+                    limit
+                ));
+            }
+            config.limit = Some(
+                usize::try_from(limit)
+                    .with_context(|| format!("Can't convert {:?} to usize", limit))?,
+            )
+        }
+
+        Ok(config)
+    }
+}
+
+fn map_git2_not_found_to_none<E>(
+    config_result: Result<E, git2::Error>,
+) -> core::result::Result<Option<E>, git2::Error> {
+    config_result
+        .map(|value| Some(value))
+        .or_else(|err| match err.code() {
+            git2::ErrorCode::NotFound => Ok(None),
+            _ => Err(err),
+        })
 }
